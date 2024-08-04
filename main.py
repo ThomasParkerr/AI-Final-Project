10694fa61b624ee7c627b35901b55e1727cf53b9
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-04 16:50:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from trackers import Tracker
 import cv2
 import numpy as np
 from team_assigner import TeamAssigner
-from player_ball_assigner import PlayerBallAssigner
-from camera_movement_estimator import CameraMovementEstimator
-from speed_and_distance_estimator import SpeedAndDistanceEstimator  # Updated import
 
 def main():
     # Read Video
@@ -17,17 +14,7 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=False,
                                        stub_path='stubs/track_stubs.pkl')
-    # Get object positions 
-    tracker.add_position_to_tracks(tracks)
-
-    # Interpolate Ball Positions
-    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
-
-    # Speed and Distance Estimator
-    # speed_and_distance_estimator = SpeedAndDistanceEstimator()
-    # speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
-
-    # Assign Player Teams
+    
     team_assigner = TeamAssigner()
     team_assigner.assign_team_color(video_frames[0], 
                                     tracks['players'][0])
@@ -40,31 +27,7 @@ def main():
             tracks['players'][frame_num][player_id]['team'] = team 
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
-    # Assign Ball Acquisition
-    player_assigner = PlayerBallAssigner()
-    team_ball_control = []
-    for frame_num, player_track in enumerate(tracks['players']):
-        ball_bbox = tracks['ball'][frame_num][1]['bbox']
-        assigned_player = player_assigner.assign_player_ball(player_track, ball_bbox)
-        
-        if assigned_player != -1:
-            tracks['players'][frame_num][assigned_player]['has_ball'] = True
-            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
-        else:
-            team_ball_control.append(team_ball_control[-1] if team_ball_control else None)
-    team_ball_control = np.array(team_ball_control)
-
-    # Draw output
-    # Draw object Tracks
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
-
-    # Draw Camera Movement
-    # output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
-
-    # Draw Speed and Distance
-    # output_video_frames = speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
-
-    # Save Video
+    #Save Video
     save_video(output_video_frames, 'output_videos/Results.mp4')
 
 if __name__ == '__main__':
